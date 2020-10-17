05f311d2a30f78e9249e190697074f3872f2d772
Camilo Anzola <ca.anzola@uniandes.edu.co>
2020-10-17 00:56:52 -0500

    Open camera

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  photo:any;
+
+  constructor(private camera: Camera) {
+
+    const options: CameraOptions = {
+
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      this.photo = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+
+  }
+
+  openCamera(){
+
+  }
+
+}
